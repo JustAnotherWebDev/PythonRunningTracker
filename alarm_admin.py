bcddf806b66b84a91abfa7a07ffbdfeacf06bd77
JustClueless <git@justclueless.bulc.club>
2020-10-06 02:01:59 +0200

    alarm notifier is running

--- alarm_admin.py
+++ alarm_admin.py
@@ -3,10 +3,11 @@ import json
 
 json_file = open('../WebScrapVolleyball/util/security.json')
 API_KEY = json.load(json_file)["telegram_token"]
+json_file = open('../WebScrapVolleyball/util/security.json')
 CHAT_ID = json.load(json_file)["dev_chatid"]
 
 def telegram_bot_sendtext():
-  bot_message = 'The Program came to a halt! Do something...'
-  send_text = f'https://api.telegram.org/bot{API_KEY}/sendMessage?chat_id=' {CHAT_ID} '&parse_mode=Markdown&text=' + bot_message
+  bot_message = 'The BeachVolleyBall-Scanner came to a halt! Do something...'
+  send_text = f'https://api.telegram.org/bot{API_KEY}/sendMessage?chat_id={CHAT_ID}&parse_mode=Markdown&text={bot_message}'
   response = requests.get(send_text)
   return response.json()
--- monitor.py
+++ monitor.py
@@ -1,16 +1,24 @@
 import psutil
 from alarm_admin import telegram_bot_sendtext
+import time
 
-process_name = ""
-
-run = true
-
-while run == true:
-  list = []
-  for process in psutil.process_iter():
-    list.append(process.name())
-  if process_name not in list:
+run = True #hier auf true setzen, damit das Programm läuft!
+print('Monitor of BeachVolleyBall-Scanner was started...')
+while run == True:
+  #time.sleep(60)
+  process_found = False
+  for pid in psutil.pids():
+    p = psutil.Process(pid)
+    try:
+      if p.cmdline()[1] == 'src/app.py':
+        process_found = True
+        print()
+    except IndexError:
+      pass # Not every pid-Stream hast more than 1 Array-Element  
+  if process_found == False:
     telegram_bot_sendtext()
-    run = false
-
-
+    time.sleep(3600)
+    telegram_bot_sendtext()
+    time.sleep(7200)
+    telegram_bot_sendtext()
+    run = False
