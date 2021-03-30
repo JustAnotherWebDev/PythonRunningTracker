8ede9b7cef9d13f1a20919e872e656d9f9623b77
clueless <git@justclueless.bulc.club>
2021-03-30 20:41:42 +0200

    Änderung der Timeouts

--- monitor.py
+++ monitor.py
@@ -2,23 +2,27 @@ import psutil
 from alarm_admin import telegram_bot_sendtext
 import time
 
-run = True #hier auf true setzen, damit das Programm läuft!
+run = True  # hier auf true setzen, damit das Programm läuft!
 print('Monitor of BeachVolleyBall-Scanner was started...')
 while run == True:
-  #time.sleep(60)
-  process_found = False
-  for pid in psutil.pids():
-    p = psutil.Process(pid)
-    try:
-      if p.cmdline()[1] == 'src/app.py':
-        process_found = True
-        print()
-    except IndexError:
-      pass # Not every pid-Stream hast more than 1 Array-Element  
-  if process_found == False:
-    telegram_bot_sendtext()
-    time.sleep(3600)
-    telegram_bot_sendtext()
-    time.sleep(7200)
-    telegram_bot_sendtext()
-    run = False
+    # time.sleep(60)
+    process_found = False
+    for pid in psutil.pids():
+        p = psutil.Process(pid)
+        try:
+            if p.cmdline()[1] == 'src/app.py':
+                process_found = True
+                print()
+        except IndexError:
+            pass  # Not every pid-Stream hast more than 1 Array-Element
+    if process_found == False:
+        telegram_bot_sendtext()
+        time.sleep(3600)
+        telegram_bot_sendtext()
+        time.sleep(7200)
+        telegram_bot_sendtext()
+        time.sleep(7200)
+        telegram_bot_sendtext()
+        time.sleep(7200)
+        telegram_bot_sendtext()
+        run = False
