cc8353c937df0609c8b8ead5ce96d0f4370e94f5
florian <fr646740@fh-muenster.de>
2021-05-11 10:30:05 +0200

    added timestamp and added counter

--- monitor.py
+++ monitor.py
@@ -1,21 +1,26 @@
 import psutil
 from alarm_admin import telegram_bot_sendtext
 import time
+import datetime
 
 run = True #hier auf true setzen, damit das Programm läuft!
 print('Monitor of BeachVolleyBall-Scanner was started...')
+counter = 0
 while run == True:
-  #time.sleep(60)
+  time.sleep(60)
   process_found = False
   for pid in psutil.pids():
     p = psutil.Process(pid)
     try:
       if p.cmdline()[1] == 'src/app.py':
         process_found = True
-        print()
+        counter = 0
+        print('Program still running at: ', datetime.datetime.now().time.strftime('%Y-%m-%d %H:%M:%S'))
     except IndexError:
+      counter += 1
+
       pass # Not every pid-Stream hast more than 1 Array-Element  
-  if process_found == False:
+  if counter > 10:
     telegram_bot_sendtext()
     time.sleep(3600)
     telegram_bot_sendtext()
