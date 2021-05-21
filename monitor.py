77fb46f34a11ae076222ee58fcaf33250a62e76b
florian <fr646740@fh-muenster.de>
2021-05-21 09:36:38 +0200

    bugfixing and improving usability

--- monitor.py
+++ monitor.py
@@ -6,24 +6,28 @@ import datetime
 run = True #hier auf true setzen, damit das Programm läuft!
 print('Monitor of BeachVolleyBall-Scanner was started...')
 counter = 0
-while run == True:
-  time.sleep(60)
-  process_found = False
-  for pid in psutil.pids():
-    p = psutil.Process(pid)
-    try:
-      if p.cmdline()[1] == 'src/app.py':
-        process_found = True
-        counter = 0
-        print('Program still running at: ', datetime.datetime.now().time.strftime('%Y-%m-%d %H:%M:%S'))
-    except IndexError:
+while True == True:
+  while run == True:
+    time.sleep(60)
+    process_found = False
+    for pid in psutil.pids():
+      p = psutil.Process(pid)
+      try:
+        if p.cmdline()[1] == 'src/app.py':
+          process_found = True
+          counter = 0
+          print('Program still running at: ', datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+      except IndexError:
+        pass # Not every pid-Stream has an Array-Element  
+    if process_found == False:
       counter += 1
-
-      pass # Not every pid-Stream hast more than 1 Array-Element  
-  if counter > 10:
-    telegram_bot_sendtext()
-    time.sleep(3600)
-    telegram_bot_sendtext()
-    time.sleep(7200)
-    telegram_bot_sendtext()
-    run = False
+    if counter > 30:
+      print('Program has ended because counter > 30 at: ', datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+      telegram_bot_sendtext()
+      time.sleep(3600)
+      telegram_bot_sendtext()
+      time.sleep(7200)
+      telegram_bot_sendtext()
+      run = False
+  time.sleep(172.800)
+  print('Program has awaken again after 48h at: ', datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
